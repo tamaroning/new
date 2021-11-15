411b7fa1bf722fdbcb61ad87eee8e41700b50100
tamaron <tamaron1203@gmail.com>
2021-11-16 01:21:09 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Router from 'vue-router'
+//import Router from 'vue-router'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 let app = createApp(App).use(router)
-app.use(Router)
+//app.use(Router)
 app.use(ElementPlus)
+app.use(router)
 
 app.mount('#app')
