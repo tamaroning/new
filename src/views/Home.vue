ed205c5af0f0f46a11326978e5b0901edb07ab38
tamaron <tamaron1203@gmail.com>
2021-11-26 23:28:51 +0900

    rebuild

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <el-main>
       <el-image
         style="width: 260px; height: 260px"
-        src="https://www.tamaron.dev/img/icon-600px.png"
+        src="https://tamaroning.github.io/img/icon-600px.png"
         fit="fill"
       ></el-image>
       <h2>tamaron</h2>
