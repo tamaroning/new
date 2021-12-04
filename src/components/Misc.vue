5330fb4a57c29c07da2a0e63437bc1b155f9097a
tamaron <tamaron1203@gmail.com>
2021-12-04 20:49:45 +0900

    update

--- src/components/Misc.vue
+++ src/components/Misc.vue
@@ -24,6 +24,14 @@
         <li><p>xv6を読む</p></li>
         <li><p>OSS開発</p></li>
       </ul>
+      <h2>自作言語の構想</h2>
+      <ul>
+        <li><p>静的型付け言語</p></li>
+        <li><p>VM上で動く</p></li>
+        <li><p>メモリ管理はARCかGC</p></li>
+        <li><p>プロセスメモリのHeapを利用してヒープとスタックを使う</p></li>
+        <li><p>最適化あり</p></li>
+      </ul>
     </el-main>
   </el-container>
 </template>
