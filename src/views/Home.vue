5043ec298f9a3fa5423f9ad4b741c97234b767dd
tamaron <tamaron1203@gmail.com>
2021-12-26 22:46:38 +0900

    deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <el-tabs tab-position="left">
       <el-tab-pane label="Top"><Top /></el-tab-pane>
       <el-tab-pane label="Profile"><Portfolio /></el-tab-pane>
-      <el-tab-pane label="Ceylon"><Ceylon /></el-tab-pane>
+      <!--<el-tab-pane label="Ceylon"><Ceylon /></el-tab-pane>-->
       <el-tab-pane label="Misc"><Misc /></el-tab-pane>
     </el-tabs>
   </el-container>
@@ -11,7 +11,7 @@
 <script>
 import Top from "../components/Top.vue";
 import Portfolio from "../components/Portfolio.vue";
-import Ceylon from "../components/Ceylon.vue";
+//import Ceylon from "../components/Ceylon.vue";
 import Misc from "../components/Misc.vue";
 
 export default {
@@ -19,7 +19,7 @@ export default {
   components: {
     Top,
     Portfolio,
-    Ceylon,
+    //Ceylon,
     Misc,
   },
 };
