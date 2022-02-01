8f8f17b7c2ec1d0d5c4bd59623047aacee64a032
tamaron <tamaron1203@gmail.com>
2022-02-01 14:39:10 +0900

    minor change

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,19 +1,21 @@
 <template>
   <el-container>
     <el-main>
-      <h2>インターンなど</h2>
+      <h2>活動</h2>
       <ul>
-        <li><p>2022年3月(3 weeks) エンジニアインターン at Wantedly, Inc.</p></li>
-        <li><p>2021年9月~現在 CSE at Coincheck, Inc.</p></li>
-        <li><p>2021年8月~9月(1 month) インターン at Coincheck, Inc.</p></li>
-      </ul>
-
-      <h2>活動など</h2>
-      <ul>
-          <li><p>セキュリティキャンプ全国大会2021 脅威解析トラック 修了</p></li>
+        <li><p>セキュリティキャンプ全国大会2021 脅威解析トラック 修了</p></li>
         <li><p>SecHack365 Trainee 2期生 修了</p></li>
         <li><p>アプリ甲子園2018 総務大臣賞</p></li>
         <li><p>Unityインターハイ2018 ゴールドアワード</p></li>
+      </ul>
+
+      <h2>インターン</h2>
+      <ul>
+        <li>
+          <p>2022年3月(3 weeks) エンジニアインターン at Wantedly, Inc.</p>
+        </li>
+        <li><p>2021年9月~現在 CSE at Coincheck, Inc.</p></li>
+        <li><p>2021年8月~9月(1 month) インターン at Coincheck, Inc.</p></li>
       </ul>
 
       <h2>OSSコントリビューション</h2>
